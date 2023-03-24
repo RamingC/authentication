862dcc8367a3a69e525428956da797336d71a4e3
RamingC <tongraming@gmail.com>
2023-03-24 16:08:55 +0700

    AuthenServer

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -18,7 +18,8 @@ const getCookie = (name) => {
   }
   return null
 }
-
+// NOTE: Authentication.
+const loginForm = document.getElementById('login-form')
 // Load the saved checkbox state from the cookie
 if (getCookie('rememberMe') === 'true') {
   const rememberMe = document.querySelector('#remember-me')
@@ -27,8 +28,7 @@ if (getCookie('rememberMe') === 'true') {
   loginForm.password.value = getCookie('password')
 }
 
-// NOTE: Authentication.
-const loginForm = document.getElementById('login-form')
+
 
 const handleSubmitLogin = (event) => {
   event.preventDefault()
